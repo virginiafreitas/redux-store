1d1674928a485efb77d6ce2d13b143f3c2c77062
virginiafreitas <virginiacdefreitas@gmail.com>
2023-09-12 08:18:57 -0700

    utils - store.js

--- client/src/pages/Detail.jsx
+++ client/src/pages/Detail.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Cart from '../components/Cart';
 // import { useState } from '../utils/GlobalState';
--- client/src/utils/store.js
+++ client/src/utils/store.js
@@ -1,6 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { createStore } from 'redux';
+import { reducer } from './reducers';
 
-export default configureStore({
-  reducer: {},
-})
+// import { configureStore } from '@reduxjs/toolkit'
 
+const store = createStore(reducer);
+
+// export default configureStore({
+//   reducer: {},
+// })
+
+export default store;
