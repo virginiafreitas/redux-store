6aeb2c37dff81ef97892210c6e84b1663b48ba31
virginiafreitas <virginiacdefreitas@gmail.com>
2023-09-11 19:59:24 -0700

    detail.jsx

--- client/src/pages/Detail.jsx
+++ client/src/pages/Detail.jsx
@@ -3,7 +3,7 @@ import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
 import Cart from '../components/Cart';
-import { useState } from '../utils/GlobalState';
+// import { useState } from '../utils/GlobalState';
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
@@ -15,14 +15,17 @@ import { idbPromise } from '../utils/helpers';
 import spinner from '../assets/spinner.gif';
 
 function Detail() {
-  const [state, dispatch] = useState();
+  // const [state, dispatch] = useState();
   const { id } = useParams();
+  const { dispatch } = useDispatch();
 
   const [currentProduct, setCurrentProduct] = useState({});
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const { products, cart } = state;
+  // const { products, cart } = state;
+  const products = useSelector((state) => state.products);
+  const cart = useSelector((state) => state.cart);
 
   useEffect(() => {
     // already in global store
