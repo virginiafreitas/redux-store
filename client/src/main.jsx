a107c68bf2b1563c53962f928a67cc4783d135b5
virginiafreitas <virginiacdefreitas@gmail.com>
2023-09-12 07:53:37 -0700

    refactor main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './utils/store';
 
 import App from './App.jsx';
 import Home from './pages/Home';
@@ -41,5 +43,7 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <Provider store = {store}>
+    <RouterProvider router={router} />
+  </Provider>
 )
